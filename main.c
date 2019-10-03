d57cfcb63885813a4fbd4403eb0d14bcba38a841
rkdms0327 <55428479+rkdms0327@users.noreply.github.com>
2019-10-03 17:14:33 +0900

    complete

--- main.c
+++ main.c
@@ -5,17 +5,50 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x;
-	int y;
+	//preject 01
+	
+	int x, y, z, m;
+	int a, b, c;
+	
+	
+	x=1;
+	a=3;
+	b=4;
+	c=5;
+	z=3;
+	
+	
+	y=m*x+b;
+	y=a*x*x+b*x+c;
+	m=(x+y+z)/3;
+	
+	printf("y=%d, m=%d\n", y, m); 
+	
+	
+	//project 02
+	
+	int d=3;
+	int cond1, cond2;
+	
+	cond1=2<d&&d<5;
+	cond2=d>1||d<4&&d>3;
+	
+	printf("cond1=%d, cond2=%d\n", cond1, cond2);
+	
+	
+	
+	//project 03
+	int q;
+	int p;
 	
 	printf("input two integer : ");
-	scanf("%d %d", &x, &y);
+	scanf("%d %d", &q, &p);
 	
-	printf("+ is %i\n: ", x+y);
-	printf("- is %i\n: ", x-y);
-	printf("* is %i\n: ", x*y);
-	printf("/ is %i\n: ", x/y);
-	printf("%% is %i\n: ", x%y);
+	printf("+ is %i\n: ", q+p);
+	printf("- is %i\n: ", q-p);
+	printf("* is %i\n: ", q*p);
+	printf("/ is %i\n: ", q/p);
+	printf("%% is %i\n: ", q%p);
 	
 	
 	
